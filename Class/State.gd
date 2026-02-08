78f60f46671d42d9af4a6be851cd86ddd59a8f54
Matthew Larrobis <105086636+MoogsKotobuki@users.noreply.github.com>
2026-02-08 20:04:44 +0800

    Initial Files

---
+++ Class/State.gd
@@ -0,0 +1,17 @@
+class_name State extends Node
+
+var state_machine : StateMachine
+# Called when the node enters the scene tree for the first time.
+
+func enter() -> void:
+	pass
+	
+func exit()-> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _update(_delta: float) -> void:
+	pass
+
+func _physics_update(_delta: float) -> void:
+	pass
---
+++ Class/input_handler.gd
@@ -0,0 +1,16 @@
+class_name InputHandler
+extends Node
+
+var input_mov : Vector2
+var input_look : Vector2
+
+var InputBuffer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
