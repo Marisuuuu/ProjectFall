a50ead1d2b104867773b68c420bb3a792627bdf1
Marisuuuu <matthew.larrobis20@gmail.com>
2026-02-09 14:53:30 +0800

    IhandlerBroken and Statemachine Transition not tested

--- Model/player_control.gd
+++ Model/player_control.gd
@@ -3,4 +3,6 @@ extends StateControl
 func _update(_delta: float) -> void:
 	
 	var input = Input.get_vector("mvLeft","mvRight","mvUp","mvDown")
-	state_machine.mov = input
+	if state_machine !=null:
+		state_machine.mov = input
+		print(input)
